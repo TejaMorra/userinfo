ee32fb8894a7c0e373d9e75ad333f59b38b677a1
TejaMorra <teja.1383ui@gmail.com>
2020-12-16 16:03:23 -0800

    "Validation,auth and error handling"

--- src/controllers/user/UserController.ts
+++ src/controllers/user/UserController.ts
@@ -1,4 +1,5 @@
 import { response, Router } from "express";
+import joi from 'joi'
 
 import { UserManager } from "../../managers/user/UserManager";
 
@@ -15,6 +16,7 @@ export class UserController {
   }
 
   private init() {
+    this.router.post('/login', this.loginUser);
     this.router.get("/", this.getUsers);
     this.router.get("/:id", this.getUser);
     this.router.post("/", this.addUser);
@@ -22,12 +24,22 @@ export class UserController {
     this.router.delete("/:id", this.deleteUser);
   }
 
+  private loginUser = async (request, response, next) => {
+    try {
+      const result = await this.userManager.loginUser(request.params);
+      response.statusCode = 200;
+      response.json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
   private deleteUser = async (request, response, next) => {
     try {
       const result = await this.userManager.deleteUser(request.params);
       response.status(200).send(result);
     } catch (error) {
-      response.status(500).send({ message: error.message || error });
+      next(error)
     }
   }
 
@@ -36,7 +48,7 @@ export class UserController {
       const result = await this.userManager.updateUser({...request.params, ...request.body});
       response.status(200).send(result);
     } catch (error) {
-      response.status(500).send({ message: error.message || error });
+      next(error)
     }
   };
   private getUsers = async (request, response, next) => {
@@ -44,7 +56,7 @@ export class UserController {
       const result = await this.userManager.getUsers();
       response.status(200).send(result);
     } catch (error) {
-      response.status(500).send({ message: error.message || error });
+      next(error)
     }
   };
 
@@ -53,16 +65,26 @@ export class UserController {
       const result = await this.userManager.getUser(req.params);
       response.status(200).send(result);
     } catch (error) {
-      response.status(500).send({ message: error.message || error });
+      next(error)
     }
   };
 
   private addUser = async (req, response, next) => {
     try {
+      const schema  = joi.object({
+        firstName: joi.string().required(),
+        lastName: joi.string(),
+        role: joi.string().required(),
+        country: joi.string().required(),
+      })
+      const data = schema.validate(req.body);
+      if (data.error) {
+        next(data.error);
+      }
       const result = await this.userManager.addUser(req.body);
       response.status(200).send(result);
     } catch (error) {
-      response.status(500).send({ message: error.message || error });
+      next(error)
     }
   };
 }
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import express from "express";
 import { ApiRouting } from "./ApiRouting";
 import { sequelize } from './config/sequelize';
+import jwt from 'jsonwebtoken';
 
 sequelize.setConnection();
 
@@ -21,13 +22,62 @@ const corsOption = {
 function configureMiddleWare() {
   app.use(cors(corsOption)); 
   app.use(bodyParser.json());
+
+  
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With,  csrf-token, LinkedIn-Cookie, Content-Type, Accept, pragma, cache-control"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.header("Access-Control-Allow-Credentials", '*');
+  next();
+});
+app.options("/*", function (req, res) {
+  res.send();
+});
+
+
 }
 
+const loggingMiddleware = (req, res, next) => {
+  if (req.originalUrl.includes('/login')) {
+    next();
+  } else {
+    console.log(req.headers)
+  if (!req.headers.authorization) {
+    return res.status(401).send('Invalid Token')
+  }
+  jwt.verify(req.headers.authorization, 'secretKey', function(err, decoded) {
+    if (err) {
+      return res.status(401).send('Invalid Token')
+    }
+  });
+  next();
+}
+}
+
+process.on("uncaughtException", (err) => {
+  console.log(err)
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err)
+});
+app.use(loggingMiddleware);
 configureMiddleWare();
 
 ApiRouting.Register(app);
 
-app.use((err, req, res, next) => {});
+app.use((err, req, res, next) => {
+  if (err)
+  res.status(500).send({ message: err.message || err });
+});
 
 app.listen(port);
 console.log(`Server running at http://localhost:${port}/`);
